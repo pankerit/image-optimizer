63241c2f65e634d1d304da8d4c4d7be1288ff86c
pankerit <pankerbest@gmail.com>
2024-08-27 20:24:32 +0300

    logs

--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@ import sharp from 'sharp'
 const server = Bun.serve({
     port: 3000,
     async fetch(request) {
+        if (request.method !== 'POST') {
+            return new Response('Method Not Allowed', { status: 200 })
+        }
         const buffer = await request.arrayBuffer()
         const image = await sharp(buffer).png({ compressionLevel: 9 }).toBuffer()
         return new Response(image, { headers: { 'Content-Type': 'image/png' } })
